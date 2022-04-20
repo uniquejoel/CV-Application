6c2bd94842e257614cbb7833e3be98d1db7cd26c
uniquejoel <joelsunday683@gmail.com>
2022-04-20 15:39:24 +0100

    cv application using react

---
+++ src/components/Edu/Edu.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Edu.css'
+
+function Edu() {
+    return (
+        <div className='Edu'>
+            <div className='section'>
+                <h2 className='head line-height'>WORK EXPERIENCE</h2>
+                <h2 className='front'>FRONTEND WEB DEVELOPER</h2>
+                <h2 className='front com'>Complanet Technologies Limited</h2>
+                <h2 className='line-height com'>JOB DESCRIPTION</h2>
+
+                <ul>
+                  <li>Coded using HTML, CSS and JavaScript to develop features for both mobile and desktop platforms.</li>
+                  <li>Teamed on user-centric design strategy in translation of UX and business requirements into coded solutions.</li>
+                  <li>Designed and updated layouts to meet usability and performance requirements.</li>
+                  <li>Tested web-based product functionality and delivered iterations to customer.</li>
+                  <li>Transformed native applications into mobile-friendly products.</li>
+                  <li>Liaised between marketing and IT on web development projects integrating front-end assets to back-end systems.</li>
+                  <li>Researched emerging web technologies and trends for possible incorporation into sites.</li>
+                </ul>
+                <br/>
+
+
+                <h2 className='head line-height'>TELLER</h2>
+                <h2 className='front com'>Guarantee Trust Bank</h2>
+                <h2 className='line-height com'>JOB DESCRIPTION</h2>
+
+                <ul>
+                  <li>Calculated fees due, interest and change for customer transactions.</li>
+                  <li>Maintained friendly and professional customer interactions.</li>
+                  <li>Handled various accounting transactions.</li>
+   
+                </ul>
+                <br/>
+
+                <h2 className='head line-height'>CORE SKILLS</h2>
+                
+               
+
+                <ul>
+                  <li><h2 className='front'>HTML</h2></li>
+                  <li><h2 className='front'>CSS</h2></li>
+                  <li><h2 className='front'>BOOTSTRAP</h2></li>
+                  <li><h2 className='front'>WORDPRESS</h2></li>
+                  <li><h2 className='front'>GRAPHICS DESIGN</h2></li>
+                </ul>
+        </div>
+        </div>
+    )
+}
+
+export default Edu
---
+++ src/components/GeneralInfo/GeneralInfo.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './GeneralInfo.css'
+
+function GeneralInfo() {
+    return (
+        <div className='GeneralInfo'>
+            <div className='section1'>
+                <h2 className='head line-height'>SUNDAY</h2>
+                <h2 className='head'>JOEL OHIOLE</h2>
+                <h2 className='front'>FRONTEND WEB DEVELOPER</h2>
+                <br/>
+
+                <h2>BIO</h2>
+                <h2 className='front'>DATE OF BIRTH: 24th NOVEMBER 1997</h2>
+                <h2 className='front'>Marital Status: Single</h2>
+                <h2 className='front'>Languages: English</h2> <br />
+
+                <h2>PERSONAL PROFILE</h2>
+                <h2 className='front'>Hardworking and passionate frontend developer
+                    with incredible skills in HTML,CSS,BOOTSTRAP,
+                    and JAVASCRIPT. Always Ready to help the team
+                    achieve company goals.</h2><br />
+
+                <h2>CONTACT INFO</h2>
+                <h2 className='front'>Gmail: joelsunday683@gmail.com</h2>
+                <h2 className='front'>Phone number: +234 809 486 2070</h2>
+                <h2 className='front'>+234 703 902 1186</h2>
+                <h2 className='front'>Abeokuta, Ogun sate</h2>
+
+              
+                
+                
+                
+
+
+                
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default GeneralInfo
